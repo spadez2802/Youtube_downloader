343eb4838592caf3248864b1704b472b37c3e502
spadez2802 <76726579+spadez2802@users.noreply.github.com>
2026-05-26 16:05:21 +0700

    Ignore history.json

--- src/app.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication
-from controllers.main_window import MyDownloader
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MyDownloader()
-    window.show()
-    sys.exit(app.exec())
